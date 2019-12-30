79869e7f99e9e3f8ce9b6a30e629da0a2d9e2fb8
CodeMonkeyqaq <1242532947@qq.com>
2019-12-30 20:51:06 +0800

    添加部分实验数据

--- src/components/Subs/Experiment/Experiment1_1.vue
+++ src/components/Subs/Experiment/Experiment1_1.vue
@@ -39,11 +39,11 @@ export default {
             chartData:{
                 columns: ['数据量级', 'Oracle', 'OracleInMemory', 'TimesTenCatche','TimesTen'],
                 rows: [
-                    { '数据量级': '千级', 'Oracle': 1393, 'OracleInMemory': 1093, 'TimesTenCatche': 0.32, 'TimesTen':50},
-                    { '数据量级': '万级', 'Oracle': 3530, 'OracleInMemory': 3230, 'TimesTenCatche': 0.26, 'TimesTen':50 },
-                    { '数据量级': '十万级', 'Oracle': 2923, 'OracleInMemory': 2623, 'TimesTenCatche': 0.76, 'TimesTen':50 },
-                    { '数据量级': '百万级', 'Oracle': 1723, 'OracleInMemory': 1423, 'TimesTenCatche': 0.49, 'TimesTen':50 },
-                    { '数据量级': '千万级', 'Oracle': 3792, 'OracleInMemory': 3492, 'TimesTenCatche': 0.323, 'TimesTen':50 }
+                    { '数据量级': '千级', 'Oracle': 3, 'OracleInMemory': 1093, 'TimesTenCatche': 0.32, 'TimesTen':50},
+                    { '数据量级': '万级', 'Oracle': 7, 'OracleInMemory': 3230, 'TimesTenCatche': 0.26, 'TimesTen':50 },
+                    { '数据量级': '十万级', 'Oracle': 29, 'OracleInMemory': 2623, 'TimesTenCatche': 0.76, 'TimesTen':50 },
+                    { '数据量级': '百万级', 'Oracle': 225, 'OracleInMemory': 1423, 'TimesTenCatche': 0.49, 'TimesTen':50 },
+                    { '数据量级': '千万级', 'Oracle': 684, 'OracleInMemory': 3492, 'TimesTenCatche': 0.323, 'TimesTen':50 }
                 ]
             },
             chartSettings:{}
